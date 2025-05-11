e0cb8d196a6dcef78933c305725e13d2bcbc66f6
Fox <thefoxjones@gmail.com>
2025-05-11 12:01:04 -0500

    C O M M I T

--- public/checkhttp.js
+++ public/checkhttp.js
@@ -37,3 +37,15 @@ if (!location.hostname.startsWith('www')) {
     location.replace('https://www.' + location.href.split('://').reverse()[0])
 }
 }*/
+
+function changeUrlWithoutRefresh(newUrl) {
+  window.history.replaceState(
+      { ...window.history.state, as: newUrl, url: newUrl },
+      '',
+      newUrl
+  );
+}
+if (location.pathname.endsWith('.html')) {
+
+  changeUrlWithoutRefresh(location.pathname.split('.html')[0] + (location.search) + (location.href.split('#')[1] ? ("#" + location.href.split('#')[1]) : ''))
+}
--- public/scripts/IMPORTANT.js
+++ public/scripts/IMPORTANT.js
@@ -1,11 +1,1 @@
-function changeUrlWithoutRefresh(newUrl) {
-  window.history.replaceState(
-      { ...window.history.state, as: newUrl, url: newUrl },
-      '',
-      newUrl
-  );
-}
-if (location.pathname.endsWith('.html')) {
-
-  changeUrlWithoutRefresh(location.pathname.split('.html')[0] + (location.search) + (location.href.split('#')[1] ? ("#" + location.href.split('#')[1]) : ''))
-}
+// moved to checkhttp.js
